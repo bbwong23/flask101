d0c9f012e0c5caf9ab57653b08e7d0e208c6fcc8
Bethany Wong <bethanywong1216@gmail.com>
2020-02-23 11:10:14 -0500

    Update calculator_controller.py

---
+++ calculator_controller.py
@@ -0,0 +1,63 @@
+###########################
+# 1. import flask library 
+###########################
+import service.calculator as calculator
+from http import HTTPStatus
+
+from flask import jsonify
+
+
+###########################
+# 2. initialize your Flask application object
+###########################
+app = 
+
+
+
+###########################
+# 4. define route paths for the following functions with the specified path and method
+# 5. and parse the request to get the user_input given the request type
+###########################
+
+
+# path = '/mean', method = 'GET'
+# request type = JSON
+def mean():
+	# user_input = 
+	
+	results = calculator.mean(user_input)
+
+	return jsonify({'output':results}), HTTPStatus.OK
+
+
+# path = '/median', method = 'GET and POST'
+# request type = Query
+def median():
+	# user_input = 
+
+	user_input = list(map(int, user_input.split(',')))
+	results = calculator.median(user_input)
+
+	return jsonify({'output':results}), HTTPStatus.OK
+
+# path = '/mode', method = 'POST'
+# request type = Form
+def mode():
+	# user_input = 
+
+	user_input = list(map(int, user_input))
+	results = calculator.mode(user_input)
+
+	return jsonify({'output':results}), HTTPStatus.OK
+
+
+# path = '/status', method = 'GET'
+def status():
+	result = "Application is running"
+	return result, HTTPStatus.OK
+
+
+if __name__ == '__main__':
+	###########################
+	# 3. Start your flask app
+	###########################
